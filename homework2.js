4ddb87341951559fbc79bce8597bcd0fdf322c3f
kkoroleva <korolevakarinka22@gmail.com>
2022-03-10 18:20:33 +0300

    add generator of mockdata

--- homework2.js
+++ homework2.js
@@ -16,10 +16,33 @@ gMap.forEach(function (_, key) {
 });
 /*Задание №3*/
 var flatData = [["36", "не приватизирован", "4 собственника", "Парковая"],
-    ["45", "приватизирован", "3 собственника", "Луговая"],
-    ["56", "не приватизирован", "1 собственник", "Луговая"],
-    ["56", "приватизирован", "5 собственников", "Ленина"],
-    ["25", "не приватизирован", "2 собственника", "Ленина"]];
+    ["45", "приватизирована", "3 собственника", "Луговая"],
+    ["56", "не приватизирована", "1 собственник", "Луговая"],
+    ["56", "приватизирована", "5 собственников", "Ленина"],
+    ["25", "не приватизирована", "2 собственника", "Ленина"]];
+var maxSquare = 60;
+var maxOwners = 20;
+var streets = ['Луговая', 'Луговая', 'Парковая'];
+var catalogSize = 10;
+function generateFlat() {
+    var flat = new Array(4);
+    var square = flat[0], privatization = flat[1], owners = flat[2], street = flat[3];
+    square = String(Math.round(Math.random() * maxSquare));
+    privatization = Math.round(Math.random()) === 0 ? 'не приватизирована' : 'приватизирована';
+    owners = String(Math.round(Math.random() * maxOwners)) + ' собственников'; //по окончанию можно упороться еще
+    street = streets[Math.round(Math.random() * (streets.length - 1))];
+    return [square, privatization, owners, street];
+}
+;
+function generateCatalog() {
+    var flats = new Array(catalogSize);
+    for (var i = 0; i < catalogSize; i++) {
+        flats[i] = generateFlat();
+    }
+    console.log(flats);
+    return flats;
+}
+;
 function isSuitable(square, street) {
     if (+square <= 40 && street === 'Ленина' || +square === 56) {
         return true;
@@ -29,9 +52,9 @@ function isSuitable(square, street) {
 ;
 function checkWholeCatalog(flats) {
     flats.forEach(function (flat) {
-        var square = flat[0], privatization = flat[1], owners = flat[2], street = flat[3];
+        var square = flat[0], street = flat[3];
         console.log("\u041F\u043E\u0434\u0445\u043E\u0434\u0438\u0442 \u043B\u0438 \u043C\u043D\u0435 \u043A\u0432\u0430\u0440\u0442\u0438\u0440\u0430 \u2116".concat(flats.indexOf(flat), ": ").concat(isSuitable(square, street)));
     });
 }
 ;
-checkWholeCatalog(flatData);
+checkWholeCatalog(generateCatalog());
--- homework2.ts
+++ homework2.ts
@@ -20,10 +20,34 @@ gMap.forEach((_, key) => {
 /*Задание №3*/
 
 const flatData: Array<Array<string>> = [["36", "не приватизирован", "4 собственника", "Парковая"],
-["45", "приватизирован", "3 собственника", "Луговая"],
-["56", "не приватизирован", "1 собственник", "Луговая"],
-["56", "приватизирован", "5 собственников", "Ленина"],
-["25", "не приватизирован", "2 собственника", "Ленина"]];
+["45", "приватизирована", "3 собственника", "Луговая"],
+["56", "не приватизирована", "1 собственник", "Луговая"],
+["56", "приватизирована", "5 собственников", "Ленина"],
+["25", "не приватизирована", "2 собственника", "Ленина"]];
+
+const maxSquare : number = 60;
+const maxOwners : number = 20;
+const streets : string[] = ['Луговая', 'Луговая', 'Парковая'];
+const catalogSize : number = 10;
+
+function generateFlat() : string[] {
+    let flat : string[] = new Array(4);
+    let [square, privatization, owners, street] = flat;
+    square = String(Math.round(Math.random() * maxSquare));
+    privatization = Math.round(Math.random()) === 0? 'не приватизирована' : 'приватизирована';
+    owners = String(Math.round(Math.random() * maxOwners)) + ' собственников'; //по окончанию можно упороться еще
+    street = streets[Math.round(Math.random() * (streets.length-1))];
+    return [square, privatization, owners, street];
+};
+
+function generateCatalog() : string[][] { 
+    let flats : string[][] = new Array(catalogSize);
+    for (let i = 0; i < catalogSize; i++) {
+        flats[i] = generateFlat();
+    }
+    console.log(flats);
+    return flats;
+};
 
 function isSuitable(square: string, street: string) : boolean {
     if (+square <= 40 && street === 'Ленина' || +square === 56) {
@@ -34,10 +58,10 @@ function isSuitable(square: string, street: string) : boolean {
 
 function checkWholeCatalog(flats: Array<Array<string>>) : void {
     flats.forEach(flat => {
-        const [square, privatization, owners, street] = flat;
+        const [square, , , street] = flat;
         console.log(`Подходит ли мне квартира №${flats.indexOf(flat)}: ${isSuitable(square, street)}`);
     });
 };
 
-checkWholeCatalog(flatData);
+checkWholeCatalog(generateCatalog());
 
